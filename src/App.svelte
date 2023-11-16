b634baff4b63b8ddcd93c3358b1d78bea7be778c
Yakiyo <yakiyo1206@gmail.com>
2023-11-16 12:59:16 +0000

    feat: finish game

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,69 @@
 <script lang="ts">
   let squares: string[] = Array(9).fill("");
   let isX = true;
+  let winner: string | null = null;
+  let isStalemate = false;
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   function onClick(i: number) {
+    if (winner) return;
     if (squares[i] !== "") return;
     squares[i] = isX ? "X" : "O";
     squares = squares;
     isX = !isX;
+    winner = calculateWinner(squares);
+    isStalemate = calculateStalemate(squares);
+  }
+
+  function calculateStalemate(squares: string[]) {
+    return !squares.some((x) => x === '') && !winner;
+  }
+
+  function calculateWinner(squares: string[]) {
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 </script>
 
+<h1>Tic Tac Toe</h1>
+
 <div class="container">
   {#each squares as square, i}
-    <button on:click={() => onClick(i)}>{square}</button>
+    <button class="square" on:click={() => onClick(i)}>{square}</button>
   {/each}
 </div>
+
+<h2>
+  {#if winner}
+    Winner is player {winner}
+  {:else if isStalemate}
+    Match is over without any winner. Restart to play again.
+  {:else}
+    Current player: {isX ? "X" : "O"}
+  {/if}
+</h2>
+<button on:click={() => {
+  squares = Array(9).fill("");
+  isX = true;
+  winner = null;
+}}>Restart</button>
 
 <style>
   * {
@@ -26,7 +76,7 @@
     padding: 0;
   }
 
-  button {
+  .square {
     background: #fff;
     border: 1px solid #999;
     float: left;
