497ff072b1136e3b8aa746455e2e5398eae119c8
Yakiyo <yakiyo1206@gmail.com>
2023-11-16 11:34:01 +0000

    feat: start

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
-  import { writable, type Writable } from "svelte/store";
-  const squares: (string)[] = Array(9).fill('');
+  const squares: string[] = Array(9).fill("");
+  let isX = true;
+  function onClick(i: number) {
+    if (squares[i] !== "") return;
+    squares[i] = isX ? "X" : "O";
+    isX = !isX;
+  }
 </script>
 
-{#each squares as square, i}
-  <button>{square}</button>
-{/each}
+<div>
+  {#each squares as square, i}
+    <button class="border-solid border-8 border-gray-500 w-20 h-20"
+      >{square}</button
+    >
+  {/each}
+</div>
