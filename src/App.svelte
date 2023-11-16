92a6b366b10c8f12b66eb5ce3b2a150f3f84b376
Yakiyo <yakiyo1206@gmail.com>
2023-11-16 11:59:27 +0000

    chore: remove tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,43 @@
 <script lang="ts">
-  const squares: string[] = Array(9).fill("");
+  let squares: string[] = Array(9).fill("");
   let isX = true;
   function onClick(i: number) {
     if (squares[i] !== "") return;
     squares[i] = isX ? "X" : "O";
+    squares = squares;
     isX = !isX;
   }
 </script>
 
-<div>
+<div class="p-3">
   {#each squares as square, i}
-    <button class="border-solid border-8 border-gray-500 w-20 h-20"
-      >{square}</button
-    >
+    <button on:click={() => onClick(i)}>{square}</button>
   {/each}
 </div>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    font-family: sans-serif;
+    margin: 20px;
+    padding: 0;
+  }
+
+  button {
+    background: #fff;
+    border: 1px solid #999;
+    float: left;
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 34px;
+    height: 34px;
+    margin-right: -1px;
+    margin-top: -1px;
+    padding: 0;
+    text-align: center;
+    width: 34px;
+  }
+</style>
